91db2d5659aee7409f319834f6a1ae46f51bf6ae
Hector <hector.mendez.dev@gmail.com>
2015-09-28 19:52:55 -0400

    estructura header menu y footer

--- front-page.php
+++ front-page.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-<?php echo 'Tema Magnolia' ?>
+<div class="row">
+  <div class="col-sm-6">
+    <h3>Tema Magnolia</h3>
+  </div>
+  <div class="col-sm-6">
+    <h3>Tema Magnolia</h3>
+  </div>
+</div>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,9 +1,13 @@
+<!doctype html>
+<html>
+
 <head>
     <meta charset="utf-8">
     <title>Bootstrap, from Twitter</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Le styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -16,31 +20,39 @@
   </head>
   <body>
 
-  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
+  <div class="banda-logo">
+    <a class="navbar-brand brand" href="/magnolia/">
+      <div class="img-responsive" style="background-image: url(<?php the_field('magnolia');?>);">
+      </div>
+    </a>
+  </div>
 
-      <div class="navbar-header">
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="menu container">
+
         <button type="button" class="navbar-toogle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand brand" href="/magnolia/">
-		<div class="img-responsive" style="background-image: url(<?php the_field('magnolia');?>);">
-		</div>	
-	</a>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
 
-              <?php wp_list_pages(array(
-			'title_li' => ''
-		)); ?>
 
-          </ul>
-        </div><!--/.nav-collapse -->
+      <div class="row">
+        <div class="col-xs-10 col-xs-offset-1">
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <?php wp_list_pages(array(
+            			'title_li' => ''
+            		)); ?>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+
       </div>
 
     </div>
-  </div>
+
+  </nav>
 
   <div class="container">
